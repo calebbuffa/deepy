f5b61ad3c72d27f1a38aec0d7c99b0fac6428071
Caleb Buffa <62570757+calebbuffa@users.noreply.github.com>
2022-04-03 19:14:48 -0500

    Create postprocess.py

---
+++ src/postprocess.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from torch import nn
+from torch import Tensor
+
+class MatrixNMS(nn.Module):
+    def __init__(self):
+        super().__init__()
+        ...
+
+    def forward(self, x: Tensor) -> Tensor:
+        return
