5b83cd81b7495a32949acdfd4ae2a2d9e8fa2e28
Caleb Buffa <62570757+calebbuffa@users.noreply.github.com>
2022-04-04 10:58:34 -0500

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+from torch import Tensor
+iport torch
+
+class DistanceMetric:
+    def __init__(self, k: int) -> None:
+        self.k = k
+
+    def __call__(self, x: Tensor) -> Tensor:
+        raise NotImplementedError
+
+
+class Euclidian(DistanceMetric):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, x: Tensor) -> Tensor:
+        """
+        K nearest neighbor search using euclidian distance.
+
+        Parameters
+        ----------
+        x : Tensor
+            Input tensor. Of shape (B, C, N).
+
+        Returns
+        -------
+        Tensor
+            Indices of the k-nearest neighbors of each point.
+        """
+        inner = -2 * torch.matmul(x.permute(0, 2, 1), x)
+        xx = torch.sum(x ** 2, dim=1, keepdim=True)
+        pairwise_distance = -xx - inner - xx.permute(0, 2, 1)
+
+        return pairwise_distance.topk(k=self.k, dim=-1)[1]
+
+
+class Cosine(DistanceMetric):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, x: Tensor) -> Tensor:
+        """
+        K nearest neighbor search using cosine similarity.
+
+        Parameters
+        ----------
+        x : Tensor
+            Input tensor. Of shape (B, C, N).
+
+        Returns
+        -------
+        Tensor
+            Indices of the k-nearest neighbors of each point.
+        """
+        norm = (x * x).sum(1, keepdims=True) ** 0.5
+        x_norm = x / norm
+        similarity_matrix = x_norm.transpose(2, 1) @ x_norm
+
+        return torch.topk(similarity_matrix, k=self.k, axis=-1)[1]
