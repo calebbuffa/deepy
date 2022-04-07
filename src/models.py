eb9231f272575d67ff8611a99ffa643b419e0988
Caleb Buffa <62570757+calebbuffa@users.noreply.github.com>
2022-04-07 16:04:03 -0500

    Update models.py

--- src/models.py
+++ src/models.py
@@ -10,40 +10,38 @@ from encode import ResNetEncoder, DenseNetEncoder, UNetEncoder
 from model_blocks import *
 
 
-class UNet(nn.Module):
-    def __init__(
-        self,
-        n_classes: int = 2,
-        in_channels: int = 3,
-        **kwargs
-    ):
+class Unet(nn.Module):
+    name: str = "unet"
+
+    def __init__(self, n_channels, n_classes, backbone: str, bilinear=False):
         super().__init__()
-        bilinear = kwargs.get("bilinear", False)
-        factor = 2 if bilinear else 1
+        self.n_channels = n_channels
         self.n_classes = n_classes
-        self.encoder = UNetEncoder(in_channels=in_channels, bilinear=bilinear)
+        self.bilinear = bilinear
+        factor = 2 if bilinear else 1
 
-        self.decoder = nn.ModuleList()
-        out_channels = self.encoder.out_channels[::-1]
-        for i in range(len(out_channels)):
-            in_channel = self.encoder.out_channels[i]
-            try:
-                out_channel = out_channels[i + 1]
-                self.decoder.append(Up(in_channel, out_channel // factor, bilinear))
-            except IndexError:
-                out_channel = n_classes
-                self.outc = OutConv(in_channel, n_classes)
+        self.inc = Down(n_channels, 64)
+        self.down1(64, 128)
+        self.down2(128, 256)
+        self.down3(256, 512)
+        self.down4(512, 1024)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = Conv1x1(64, n_classes)
 
-            print(in_channel, out_channel)
-
-    def forward(self, x: Tensor) -> Tensor:
-        encoded_feature_maps = self.encoder(x)
-        x = self.decoder[0](encoded_feature_maps["x5"], encoded_feature_maps["x4"])
-        x = self.decoder[1](x, encoded_feature_maps["x3"])
-        x = self.decoder[2](x, encoded_feature_maps["x2"])
-        x = self.decoder[3](x, encoded_feature_maps["x1"])
-        out = self.outc(x)
-        return out
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        return self.outc(x)
 
 class SoloV2(nn.Module):
     def __init__(self, backbone: str, pretrained: bool, fpn_out: int) -> None:
@@ -63,100 +61,3 @@ class SoloV2(nn.Module):
         mask = self.mask_head(mask_kernel, features)
         return self.nms(mask)
     
-    
-class DGCNN(nn.Module):
-    def __init__(self, num_classes: int, k: int, emb_dims: int, dropout: float) -> None:
-        """
-        Initialize Dynamic Graph CNN.
-
-        Parameters
-        ----------
-        num_classes : int
-            Number of classes to predict.
-        k : int
-            K nearest neighbors to update graph.
-        emb_dims : int
-            Embedding dimensions.
-        dropout : float
-            Percentage of nodes to drop.
-        """
-        super().__init__()
-        self.layer1 = nn.Sequential(
-            EdgeConv(
-                in_channels=6 * 2,
-                out_channels=64,
-                k=k,
-                method=Euclidian,
-                n_convs=2,
-                batch_norm=True,
-                conv_dim=2,
-            ),
-            Pooling(method="max"),
-        )
-
-        self.layer2 = nn.Sequential(
-            EdgeConv(
-                in_channels=64 * 2,
-                out_channels=64,
-                k=k,
-                method=Cosine,
-                n_convs=2,
-                batch_norm=True,
-                conv_dim=2,
-            ),
-            Pooling(method="max"),
-        )
-
-        self.layer3 = nn.Sequential(
-            EdgeConv(
-                in_channels=64 * 2, 
-                out_channels=64,
-                k=k,
-                method=Cosine,
-                n_convs=2,
-                batch_norm=True,
-                conv_dim=2,
-            ),
-            Pooling(method="max"),
-        )
-
-        self.layer4 = nn.Sequential(
-            Convolution(
-                in_channels=64 * 3,
-                out_channels=emb_dims,
-                batch_norm=False,
-                conv_dim=1,
-            ),
-            Pooling(method="max", keep_dim=True),
-        )
-
-        self.fc = nn.Sequential(
-            nn.Conv1d(emb_dims + 192, 512, kernel_size=1, bias=False),  # 192/18
-            nn.BatchNorm1d(512),
-            nn.LeakyReLU(negative_slope=0.2),
-            nn.Conv1d(512, 256, kernel_size=1, bias=False),
-            nn.BatchNorm1d(256),
-            nn.LeakyReLU(negative_slope=0.2),
-            nn.Dropout(p=dropout),
-            nn.Conv1d(256, num_classes, kernel_size=1, bias=False),
-        )
-
-        self.concat = Concatenate()
-        self.repeat = Repeat()
-        self.adaptive_pooling = Pooling(method="max", adaptive=True, size=(64, 1))
-
-    def forward(self, x: Tensor) -> Tensor:
-        num_points = x.size(2)
-
-        x1 = self.layer1(x)
-        x2 = self.layer2(x1)
-        x3 = self.layer3(x2)
-        
-        x4 = self.concat(x1, x2, x3)
-
-        x4 = self.layer4(x4)
-        
-        x4 = self.repeat(x4, num_points)
-        x5 = self.concat(x1, x2, x3, x4)
-
-        return self.fc(x5)
