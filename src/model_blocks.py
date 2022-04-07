ecdfdd2d4c139f878de77613d13793c9af1d49a1
Caleb Buffa <62570757+calebbuffa@users.noreply.github.com>
2022-04-07 16:10:23 -0500

    Update model_blocks.py

--- src/model_blocks.py
+++ src/model_blocks.py
@@ -16,7 +16,7 @@ class Conv1x1(nn.Module):
 class Conv3x3(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, **kwargs):
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, **kwargs)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=self.kwargs.get("padding", 1), **kwargs)
         
      def forward(self, x: Tensor) -> Tensor:
         return selv.conv(x)
@@ -80,57 +80,60 @@ class Up(nn.Module):
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
-
- 
-class KernelHead(nn.Module):
-    def __init__(self):
-        super().__init__()
-        ...
-
-    def forward(self, fpn_features: OrderedDict[Tensor]) -> Tensor:
-        """
-        first resize the input feature FI ∈ RHI ×WI ×C into shape of S × S × C. 
-        Then 4×convs and a final 3 × 3 × D conv are employed to generate the kernel G. 
-        We add the spatial functionality to FI by giving the first convolution access to
-        the normalized coordinates following CoordConv [23], i.e., concatenating two
-        additional input channels which contains pixel coordinates normalized to 
-        [−1, 1]. Weights for the head are shared across different feature map levels.
-        For each grid, the kernel branch predicts the D-dimensional output to indicate
-        predicted convolution kernel weights, where D is the number of parameters. 
-        For generating the weights of a 1×1 convolution with E input channels, D equals
-        E. As for 3×3 convolution, D equals 9E. These generated weights re conditioned
-        on the locations, i.e., the grid cells. If we divide the input image into S×S
-        grids, the output space will be S×S×D, There is no activation function on the 
-        output.
-        """
-        return 
-
-class FeatureHead(nn.Module):
-    def __init__(self):
-        super().__init__()
-        ...
     
-    def forward(self, fpn_features: OrderedDict[Tensor]) -> Tensor:
-        """
-        After repeated stages of 3 × 3 conv, group norm [ 34 ], ReLU and 2× bilinear 
-        upsampling, the FPN features P2 to P5 are merged into a single output at 1/4 
-        scale. The last layer after the element-wise summation consists of 1 × 1 
-        convolution, group norm and ReLU. More details can be referred to supplementary 
-        material. It should be noted that we feed normalized pixel coordinates to the 
-        deepest FPN level (at 1/32 scale), before the convolutions and bilinear 
-        upsamplings.
-        """
-        return
+class Atrous3x3Conv(nn.Module):
+    def __init__(self, in_channels, out_channels, dilation_rate, **kwargs):
+        super(Atrous3x3Conv, self).__init__()
+        self.conv = Conv3x3(
+            in_channels,
+            out_channels,
+            padding=dilation_rate,
+            dilation=dilation_rate,
+            **kwargs
+        )
 
+    def forward(self, x):
+        return self.conv(x)
 
-class MaskHead(nn.Module):
-    def __init__(self):
+class ASPPHead(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, rate: int):
         super().__init__()
-        ...
-    
-    def forward(self, mask_kernel, features):
-        # convolve mask kernel over features
-        return
+        self.block = nn.Sequential(
+             Atrous3x3Conv(in_channels, out_channels, rate, bias=False),
+             nn.BatchNorm2d(out_channels),
+             nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+
+class ASPP(nn.Module):
+    dilation_rates = [6, 12, 18]
+
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+        self.conv1 = Conv1x1(in_channels, out_channels)
+        self.asp1 = ASPPHead(in_channels, out_channels, self.dilation_rates[0])
+        self.asp2 = ASPPHead(in_channels, out_channels, self.dilation_rates[1])
+        self.asp3 = ASPPHead(in_channels, out_channels, self.dilation_rates[2])
+        self.pool = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            COnv1x1(in_channels, out_channels, bias=False),
+            nn.ReLU(inplace=True),
+        )
+        self.outc = Conv1x1(in_channels * 5, out_channels)
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = self.asp1(x)
+        x3 = self.asp2(x)
+        x4 = self.asp3(x)
+        x5 = self.pool(x)
+        x5 = F.interpolate(x5, size=x4.shape[2:], mode='bilinear', align_corners=True)
+        x = torch.cat([x1, x2, x3, x4, x5], dim=1)
+        return self.outc(x)
 
 
 class SkipConnection:
